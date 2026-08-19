deae047c3a8addf5b6bb76507ab989911dab6d73
LARISSA <adiltoleao.2017@gmail.com>
2026-08-18 23:50:43 -0300

    feat: atualiza tabelas

--- apps/services/core-service/alembic/env.py
+++ apps/services/core-service/alembic/env.py
@@ -4,27 +4,33 @@ from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 
-# Importar modelos e configurações
 from app.core.config import settings
-from app.models.tutorial import Base
+from app.models.base import Base
 
-# Objeto de configuração do Alembic
+from app.models.usuario import Usuario
+from app.models.cidades import Cidade
+from app.models.hotel import Hotel
+
+
 config = context.config
 
-# Configurar o sistema de logs
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# Definir a URL do banco de dados dinamicamente a partir das Configurações
-config.set_main_option("sqlalchemy.url", settings.SQLALCHEMY_DATABASE_URI)
 
-# Objeto Metadata para geração automática de migrações
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.SQLALCHEMY_DATABASE_URI,
+)
+
+
 target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode."""
     url = config.get_main_option("sqlalchemy.url")
+
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -37,7 +43,6 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode."""
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
@@ -45,7 +50,10 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+        )
 
         with context.begin_transaction():
             context.run_migrations()
